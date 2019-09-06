8d58086cf5ede3083c0dccfb0784c449b583d6a6
Bhupen Pal <bhupen16pal@gmail.com>
2019-09-07 03:48:37 +0530

    Half Home

---
+++ controllers/landing.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const Router = express.Router();
+
+const queryModel = require("../models/query.js");
+
+Router.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+Router.get("/FreeTrial", (req, res) => {
+  res.render("freeTrialFull.ejs");
+});
+
+Router.get("/Products", (req, res) => {
+  res.render("productFull.ejs");
+});
+
+Router.get("/Offers", (req, res) => {
+  res.render("offerFull.ejs");
+});
+
+Router.get("/ContactUs", (req, res) => {
+  res.render("contactFull.ejs");
+});
+
+module.exports = Router;
---
+++ start.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+
+const landing = require("./controllers/landing.js");
+app.use("/", landing);
+app.use("/assets", express.static("assets"));
+
+app.listen(8080, () => console.log("The server is started at PORT: 8080"));
