0477d54bf49b071745cf427d5d17f973f79fad3b
Bhupen Pal <bhupen16pal@gmail.com>
2019-09-08 01:03:59 +0530

    Forms Ready

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const Router = express.Router();
 
-const queryModel = require("../models/query.js");
+const queryModel = require("../models/qModel.js");
+const trialModel = require("../models/tModel.js");
 
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Get Request (INDEX, FREETRIAL, PRODUCTS, OFFERS, CONTACTUS)
 Router.get("/", (req, res) => {
   res.render("index.ejs");
 });
@@ -12,15 +17,47 @@ Router.get("/FreeTrial", (req, res) => {
 });
 
 Router.get("/Products", (req, res) => {
-  res.render("productFull.ejs");
+  res.render("proDucts.ejs");
 });
 
 Router.get("/Offers", (req, res) => {
-  res.render("offerFull.ejs");
+  res.render("offers.ejs");
 });
 
 Router.get("/ContactUs", (req, res) => {
-  res.render("contactFull.ejs");
+  res.render("contactUs.ejs");
+});
+
+// POST Request (FREETRIAL, CONTACTUS)
+Router.post("/FreeTrial", urlencodedParser, (req, res) => {
+  let newTrial = new trialModel();
+  (newTrial.custName = req.body.custName),
+    (newTrial.custNumb = req.body.custNumb),
+    (newTrial.carName = req.body.carName),
+    (newTrial.carNumb = req.body.carNumb),
+    newTrial.save(err => {
+      if (err) {
+        console.log(err, "error");
+        return;
+      }
+      res.redirect("/");
+    });
+});
+
+Router.post("/ContactUs", urlencodedParser, (req, res) => {
+  let newQuery = new queryModel();
+  (newQuery.custfName = req.body.custfName),
+    (newQuery.custlName = req.body.custlName),
+    (newQuery.custNumb = req.body.custNumb),
+    (newQuery.custEmail = req.body.Email),
+    (newQuery.messAge = req.body.messAge),
+    newQuery.save(err => {
+      if (err) {
+        console.log(err, "error");
+        return;
+      }
+      res.redirect("/");
+    });
 });
 
 module.exports = Router;
--- models/qModel.js
+++ models/qModel.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/DazzlingWheels", {
+  useNewUrlParser: true
+});
+
 const Schema = mongoose.Schema;
 
 const querySchema = new Schema({
-  custName: String,
+  custfName: String,
+  custlName: String,
   custNumb: Number,
+  custEmail: String,
   messAge: String
 });
 
