bd6bfba44a09cc86db79134d8aaff64bae328efa
Bhupen Pal <bhupen16pal@gmail.com>
2019-10-06 21:39:57 +0530

    Almost Done

--- models/qModel.js
+++ models/qModel.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/DazzlingWheels", {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 const Schema = mongoose.Schema;
