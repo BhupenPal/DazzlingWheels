243c948ba373f67f682a143a3880a0980b2b323c
Pooja Goyal <poojagl85>
2019-10-07 19:59:05 +0530

    navbar and contact us updated

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,23 @@
-function menuOpener(e){e.classList.toggle("change"),document.getElementById("repeatCross").classList.toggle("change"),document.getElementById("sideNav").style.width="40%",document.getElementById("sideNav").style.opacity="1"}function menuCloser(e){e.classList.toggle("change"),document.getElementById("origCross").classList.toggle("change"),document.getElementById("sideNav").style.width="0",document.getElementById("sideNav").style.opacity="0"}AOS.init();
+function menuOpener(e) {
+  e.classList.toggle("change"),
+    document.getElementById("repeatCross").classList.toggle("change"),
+    (document.getElementById("sideNav").style.width = "40%"),
+    (document.getElementById("sideNav").style.opacity = "1"),
+    (document.getElementById("sbtn-home").disabled = false),
+    (document.getElementById("sbtn-freetrial").disabled = false),
+    (document.getElementById("sbtn-products").disabled = false),
+    (document.getElementById("sbtn-offers").disabled = false),
+    (document.getElementById("sbtn-contactus").disabled = false);
+}
+function menuCloser(e) {
+  e.classList.toggle("change"),
+    document.getElementById("origCross").classList.toggle("change"),
+    (document.getElementById("sideNav").style.width = "0"),
+    (document.getElementById("sideNav").style.opacity = "0"),
+    (document.getElementById("sbtn-home").disabled = true),
+    (document.getElementById("sbtn-freetrial").disabled = true),
+    (document.getElementById("sbtn-products").disabled = true),
+    (document.getElementById("sbtn-offers").disabled = true),
+    (document.getElementById("sbtn-contactus").disabled = true);
+}
+AOS.init();
